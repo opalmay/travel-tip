9d76c1e85081f6860c20bad1662fb83c76692cbf
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-17 16:48:35 +0300

    add weather icons

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
 import { weatherService } from './services/weather.service.js'
+import { icons } from './icons.js';
 
 function getLocation() {
     locService.getLocs()
@@ -22,7 +23,7 @@ function onGoToMyLocation() {
 function onGoToLocation(lat, lng) {
     mapService.panTo({ lat, lng });
 }
-function renderWeather(weather){
+function renderWeather(weather) {
     let elWeather = document.querySelector('.weather');
     elWeather.querySelector('.weatherFirstLine').innerText = `${weather.name},${weather.sys.country} ${weather.description}`;
     elWeather.querySelector('.weatherSecondLine').innerText = `${weather.main.temp}, temperature from ${weather.main.temp_min} to ${weather.main.temp_max}C,
@@ -30,9 +31,21 @@ function renderWeather(weather){
 }
 window.onload = () => {
     // document.querySelector('myLocation').addEventListener('click', onGoToMyLocation);
-    weatherService.getWeather(32.0749831,34.9120554).then((ans)=>{
+    weatherService.getWeather(32.0749831, 34.9120554).then((ans) => {
         console.log(ans)
         renderWeather(ans);
+        var weatherIcons = JSON.parse(icons);
+        var prefix = 'wi-';
+        var code = ans.weather[0].id;
+        var icon = weatherIcons[code].icon;
+
+        if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+        }
+
+        icon = prefix + icon;
+        console.log(icon);
+        document.querySelector('img').src = `svg/${icon}.svg`;
     });
     mapService.initMap()
         .then(() => {
