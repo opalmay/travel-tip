3dcef7e3a69d2fc7cba52d2bd38646b4dc5944c2
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-17 21:17:13 +0300

    REAL FINAL

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ function renderWeather(weather) {
     }
 }
 function onCopyLocation() {
-    navigator.clipboard.writeText(`opalmay.github.io/travel-tip/index.html?lat=${gLastLocation.lat}&lng=${gLastLocation.lng}`);
+    navigator.clipboard.writeText(`opalmay.github.io/travel-tip/index.html?lat=${lastLocation.lat}&lng=${lastLocation.lng}`);
     // navigator.clipboard.writeText(`http://127.0.0.1:5500/index.html?lat=${lastLocation.lat}&lng=${lastLocation.lng}`);
     showToast();
 }
