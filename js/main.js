4e75c606e2d0d033e32f32f63ce9bff9d2066c65
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-06-17 16:10:42 +0300

    added weather service

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
+import { weatherService } from './services/weather.service.js'
 
 function getLocation() {
     locService.getLocs()
@@ -19,30 +20,32 @@ function onGoToMyLocation() {
     renderCurrentLocation(location);
 }
 function onGoToLocation(lat, lng) {
-    mapService.addMarker({ lat, lng });
+    mapService.panTo({ lat, lng });
 }
-
-
-// document.querySelector('.btn').addEventListener('click', (ev) => {
-//     console.log('Aha!', ev.target);
-//     mapService.panTo(35.6895, 139.6917);
-// })
-
+function renderWeather(weather){
+    let elWeather = document.querySelector('.weather');
+    elWeather.querySelector('.weatherFirstLine').innerText = `${weather.name},${weather.sys.country} ${weather.description}`;
+    elWeather.querySelector('.weatherSecondLine').innerText = `${weather.main.temp}, temperature from ${weather.main.temp_min} to ${weather.main.temp_max}C,
+    wind ${weather.wind.speed} m/s`;
+}
 window.onload = () => {
-    document.querySelector('myLocation').addEventListener('click', onGoToMyLocation);
-    // mapService.initMap()
-    //     .then(() => {
+    // document.querySelector('myLocation').addEventListener('click', onGoToMyLocation);
+    weatherService.getWeather(32.0749831,34.9120554).then((ans)=>{
+        console.log(ans)
+        renderWeather(ans);
+    });
+    mapService.initMap()
+        .then(() => {
+            mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+        })
+        .catch(console.log('INIT MAP ERROR'));
 
-    //         mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
-    //     })
-    //     .catch(console.log('INIT MAP ERROR'));
+    locService.getPosition()
+        .then(pos => {
 
-    // locService.getPosition()
-    //     .then(pos => {
-
-    //         console.log('User position is:', pos.coords);
-    //     })
-    //     .catch(err => {
-    //         console.log('err!!!', err);
-    //     })
+            console.log('User position is:', pos.coords);
+        })
+        .catch(err => {
+            console.log('err!!!', err);
+        })
 }
--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -1,1 +1,24 @@
 //3bde70a5cfe0bfcf4827a51e338cf54f
+//http://api.openweathermap.org/data/2.5/weather?lat=32.0749831&lon=34.9120554&appid=3bde70a5cfe0bfcf4827a51e338cf54f
+
+export const weatherService = {
+    getWeather
+}
+export function getWeather(lat, lon) {
+    return new Promise((resolve, reject) => {
+        var httpRequest = new XMLHttpRequest();
+
+        httpRequest.onreadystatechange = () => {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if(httpRequest.status === 200){
+                const ans = JSON.parse(httpRequest.responseText);
+                resolve(ans);
+                }
+                
+            }
+        }
+
+        httpRequest.open('GET', `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=3bde70a5cfe0bfcf4827a51e338cf54f`, true);
+        httpRequest.send();
+    });
+}
