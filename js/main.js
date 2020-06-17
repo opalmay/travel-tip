43acb25b163674c119c4fecf9794fae494bb405b
Palachy <Oren.Palachy@lamresearch.com>
2020-06-17 18:47:55 +0300

    Location

--- js/main.js
+++ js/main.js
@@ -3,42 +3,66 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
 import { weatherService } from './services/weather.service.js'
+import { icons } from './icons.js';
 
-function getLocation() {
-    locService.getLocs()
-        .then(locs => {
-            console.log('locs', locs)
-            return locs;//todo coords to place name
-        })
-}
-function renderCurrentLocation(locationName) {
-    document.querySelector('.currLocation span').innerText = locationName;
-}
+
 function onGoToMyLocation() {
-    let location = getLocation();
-    onGoToLocation(location.latitude, location.longtitude);
-    renderCurrentLocation(location);
+    locService.getPosition().then(location => onGoToLocation(location.coords.latitude, location.coords.longitude))
 }
 function onGoToLocation(lat, lng) {
     mapService.panTo({ lat, lng });
+    mapService.addMarker({ lat, lng });
+    renderLocation('test');
+    weatherService.getWeather(lat, lng).then((weather) => renderWeather(weather));
 }
+<<<<<<< HEAD
+=======
+function renderLocation(locationName) {
+    document.querySelector('.currLocation span').innerText = locationName;
+}
+>>>>>>> c07b8cfe52ea1ad62a583a25e02a88c1fe5a82e6
 function renderWeather(weather) {
     let elWeather = document.querySelector('.weather');
-    elWeather.querySelector('.weatherFirstLine').innerText = `${weather.name},${weather.sys.country} ${weather.description}`;
-    elWeather.querySelector('.weatherSecondLine').innerText = `${weather.main.temp}, temperature from ${weather.main.temp_min} to ${weather.main.temp_max}C,
+
+    let firstLine = elWeather.querySelector('.weatherFirstLine');
+    firstLine.innerHTML = `<span>${weather.name}, ${weather.sys.country}</span> ${weather.weather[0].description}`;
+
+    let secondLine = elWeather.querySelector('.weatherSecondLine');
+    secondLine.innerHTML = `<span>${weather.main.temp}C</span> temperature from ${weather.main.temp_min} to ${weather.main.temp_max}C,
     wind ${weather.wind.speed} m/s`;
+
+    renderIcon();
+    function renderIcon() {
+        var weatherIcons = JSON.parse(icons);
+        var prefix = 'wi-';
+        var code = weather.weather[0].id;
+        var icon = weatherIcons[code].icon;
+
+        if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+        }
+
+        icon = prefix + icon;
+        document.querySelector('img').src = `svg/${icon}.svg`;
+    }
 }
 window.onload = () => {
+<<<<<<< HEAD
     // document.querySelector('myLocation').addEventListener('click', onGoToMyLocation);
     weatherService.getWeather(32.0749831, 34.9120554).then((ans) => {
         console.log(ans)
         renderWeather(ans);
     });
+=======
+    document.querySelector('.myLocation').addEventListener('click', onGoToMyLocation);
+
+>>>>>>> c07b8cfe52ea1ad62a583a25e02a88c1fe5a82e6
     mapService.initMap()
         .then(() => {
-            mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
+            onGoToMyLocation();
         })
         .catch(console.log('INIT MAP ERROR'));
+<<<<<<< HEAD
 
     locService.getPosition()
         .then(pos => {
@@ -62,3 +86,6 @@ window.onload = () => {
 function print() {
     console.log(document.querySelector('#my-location-input').value);
 }
+=======
+}
+>>>>>>> c07b8cfe52ea1ad62a583a25e02a88c1fe5a82e6
--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -1,6 +1,3 @@
-//3bde70a5cfe0bfcf4827a51e338cf54f
-//http://api.openweathermap.org/data/2.5/weather?lat=32.0749831&lon=34.9120554&appid=3bde70a5cfe0bfcf4827a51e338cf54f
-
 export const weatherService = {
     getWeather
 }
@@ -10,11 +7,10 @@ export function getWeather(lat, lon) {
 
         httpRequest.onreadystatechange = () => {
             if (httpRequest.readyState === XMLHttpRequest.DONE) {
-                if(httpRequest.status === 200){
-                const ans = JSON.parse(httpRequest.responseText);
-                resolve(ans);
-                }
-                
+                if (httpRequest.status === 200) {
+                    const ans = JSON.parse(httpRequest.responseText);
+                    resolve(ans);
+                } else reject(ans);
             }
         }
 
